d167e32dca6b21dc2614ee850c85dfe71071c7a6
Damian <damianezetiel1@gmail.com>
2019-09-03 19:55:37 +0200

    fix left menu

--- src/components/leftMenu/leftMenu.vue
+++ src/components/leftMenu/leftMenu.vue
@@ -8,18 +8,22 @@
           @click="pageRouter(item.route)"
           class="pb-1 pt-1"
         >
-          <v-list-tile-action class="pl-1">
-            <v-icon color="#BDBDBD">{{ item.icon }}</v-icon>
+          <v-list-tile-action class="list-item">
+            <v-icon color="#DDDDDD">{{ item.icon }}</v-icon>
           </v-list-tile-action>
 
           <v-list-tile-content>
-            <v-list-tile-title :style="{ 'color' : '#BDBDBD' }">
-              <span>{{ $i18n.t(item.name).toLowerCase() }}</span>
+            <v-list-tile-title :style="{ 'color' : '#DDDDDD' }">
+              <span>{{ $i18n.t(item.name) }}</span>
             </v-list-tile-title>
           </v-list-tile-content>
         </v-list-tile>
       </v-list>
     </v-list>
+
+    <v-btn small fab class="settings">
+      <v-icon color="#BDBDBD">settings</v-icon>
+    </v-btn>
   </v-navigation-drawer>
 </template>
 
--- src/components/leftMenu/leftMenuCode.ts
+++ src/components/leftMenu/leftMenuCode.ts
@@ -1,32 +1,40 @@
-import { Component, Prop, Vue } from "vue-property-decorator";
+import { Component, Vue } from "vue-property-decorator";
 
 @Component({})
 export default class LeftMenuCode extends Vue {
     private search: string = "";
     private menu: any = {
-        configuration: [
-            { name: "inicio", icon: "home", route: "MainPage" },
-            { name: "mi perfil", icon: "people", route: "Barber" },
-            { name: "mi barberia", icon: "home", route: "BarberShop" }
+        options: [
+            { name: "Mi perfil", icon: "account_circle", route: "Barber" },
+            { name: "Buscar barberia", icon: "search", route: "MainPage" },
+            { name: "Barberias favoritas", icon: "home", route: "BarberShop" },
+            { name: "Mis reservas", icon: "event", route: "BarberShop" }
         ]
     };
 
-    get filteredConf() {
-        return this.menu.configuration.filter(option => {
-            return option.name.toLowerCase().indexOf(this.search.toLowerCase()) >= 0;
-        });
-    }
-
+    //show and hide the leftsidemenu
     get drawer() {
         return this.$store.state.drawer.left;
     }
-
     set drawer(drawer) {
         this.$store.commit("setLeftDrawer", drawer);
     }
 
+    //find the option by search menu
+    get filteredConf() {
+        return this.menu.options.filter(option => {
+            return option.name.indexOf(this.search.toLowerCase()) >= 0;
+        });
+    }
+    
+    //go to the route
     pageRouter(route: string) {
         console.log(route)
         this.$router.push(route);
     }
 }
+
+
+
+
+
