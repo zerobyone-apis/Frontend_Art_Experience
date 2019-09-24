a32bbcab8896e7943fb756e3c7a82c355a1d99a6
Damian <damianezetiel1@gmail.com>
2019-09-25 00:53:39 +0200

    fixes in barber profile

--- src/views/barber/barberPage.vue
+++ src/views/barber/barberPage.vue
@@ -1,97 +1,88 @@
 <template>
   <v-dialog v-model="model" fullscreen scrollable transition="slide-y-reverse-transition">
-    <div id="barberProfile" class="page-content white">
-      <div class="black" style="position: relative; width: 100%; height: 60px;">
-        <v-layout row wrap class="text-xs-center">
-          <v-flex xs4 xl4 sm4 class="text-xs-left pt-3 pl-1">
-            <v-icon @click="model=false" dark fab>arrow_back</v-icon>
-          </v-flex>
-          <v-flex xs4 xl4 sm4 class="pt-3">
-            <span class="white--text">{{ $i18n.t("Perfil del barbero") }}</span>
-          </v-flex>
-          <v-flex xs4 xl4 sm4></v-flex>
-        </v-layout>
+    <div class="background-dialog">
+      <div class="toolbar-dialog" :class="$vuetify.breakpoint.name+'-format'">
+        <v-icon class="pt-3 pl-1" @click="model=false" dark fab>arrow_back</v-icon>
+        <!-- <div class="barbershop-actions">
+          <v-img class="action-logo" :src="icons.facebook"></v-img>
+          <v-img class="action-logo" :src="icons.instagram"></v-img>
+          <v-btn small class="btn-reservation" v-if="false">consultar</v-btn>
+          <v-btn small class="btn-reservation" @click="dialogs.add = true">reservar</v-btn>
+        </div>-->
       </div>
 
-      <v-layout row wrap>
-        <!-- banner -->
-        <v-flex xs12 sm12 md12 xl12>
-          <div class="barbershop-banner">
-            <v-img
-              :src="barber['banner']['img']"
-              aspect-ratio="1"
-              class="barbershop-banner-wallpaper grey lighten-2"
-            >
-              <template v-slot:placeholder>
-                <v-row class="progress fill-height ma-0" align="center" justify="center">
-                  <v-progress-circular indeterminate color="grey lighten-5"></v-progress-circular>
-                </v-row>
-              </template>
-            </v-img>
+      <div class="barber-profile-page" :class="$vuetify.breakpoint.name+'-format'">
+        <!-- BANNER  -->
+        <div class="banner">
+          <v-img
+            :src="barberData.banner.img"
+            aspect-ratio="1"
+            class="banner-wallpaper grey lighten-2"
+          ></v-img>
 
-            <!-- barber.banner.img -->
-            <v-img
-              :src="barber['picture']['img']"
-              aspect-ratio="1"
-              class="barbershop-banner-logo grey lighten-2"
-            >
-              <template v-slot:placeholder>
-                <v-row class="progress fill-height ma-0" align="center" justify="center">
-                  <v-progress-circular indeterminate color="grey lighten-5"></v-progress-circular>
-                </v-row>
-              </template>
-            </v-img>
+          <v-img :src="barberData.picture.img" aspect-ratio="1" class="banner-logo grey lighten-2">
+            <template v-slot:placeholder></template>
+          </v-img>
 
-            <!-- barber.picture.img -->
-            <p class="barbershop-banner-name">{{ barber.name }}</p>
+          <div class="darkbox">
+            <p class="darkbox-name">{{ barberData.info.name }}</p>
+            <v-icon dark class="darkbox-white-stars" small>star star star star star</v-icon>
+            <v-icon class="darkbox-stars" small>{{ getStars() }}</v-icon>
           </div>
-        </v-flex>
-        <!-- main container -->
-        <v-flex xs12 sm12 md12 xl12>
-          <div class="barbershop-content">
-            <!-- actions bar -->
-            <v-layout row wrap>
-              <v-flex v-if="true" xs12 sm12 md12 xl12>
-                <div class="barbershop-actions">
-                  <v-img class="action-logo" :src="icons.facebook"></v-img>
-                  <v-img class="action-logo" :src="icons.instagram"></v-img>
-                  <v-btn small class="btn-reservation" v-if="false">consultar</v-btn>
-                  <v-btn small class="btn-reservation" @click="dialogs.add = true">reservar</v-btn>
-                </div>
-              </v-flex>
-            </v-layout>
-            <!-- content presentation -->
-            <div class="barbershop-content-presentation">
-              <v-layout v-if="true" row wrap>
-                <!-- <v-flex xs4 xl4 sm4>
-                  
-                  <v-img class="barbershop-content-something" src />
-                </v-flex> -->
-                <v-flex xs8 xl8 sm8>
-                  <!-- about the barbershop -->
-                  <div class="barbershop-content-info">
-                    <p class="font-info-title">cel:</p>
-                    <p class="font-info">{{ barber.job }}</p>
-                    <p class="font-info-title">{{ barber.email }}</p>
-                    <p class="font-info">{{ barber.info }}</p>
+        </div>
+
+        <div class="actions-box">
+          <v-img class="action-logo" :src="icons.facebook"></v-img>
+          <v-img class="action-logo" :src="icons.instagram"></v-img>
+          <v-btn small class="btn-reservation" v-if="false">consultar</v-btn>
+          <v-btn small class="btn-reservation" @click="dialogs.add = true">reservar</v-btn>
+        </div>
+
+        <!-- INFO -->
+        <div class="info-content">
+          <v-stepper v-model="wizard" vertical>
+            <v-stepper-header>
+              <v-stepper-step step="1" @click="wizard=1">informacion</v-stepper-step>
+              <v-divider></v-divider>
+              <v-stepper-step step="2" @click="wizard=2">productos</v-stepper-step>
+              <v-divider></v-divider>
+              <v-stepper-step step="3" @click="wizard=3">promociones</v-stepper-step>
+            </v-stepper-header>
+            <v-stepper-items>
+              <v-stepper-content step="1">
+                <v-card>
+                  <div class="info-content-box">
+                    <!-- about the barbershop -->
+                    <div class="barbershop-content-info">
+                      <div class="info-box">
+                        <p class="font-info-title">Cargo</p>
+                        <p class="font-info">{{ barberData.info.job }}</p>
+                        <p class="font-info-title">cel:</p>
+                        <p class="font-info">{{ barberData.info.phone }}</p>
+                        <p class="font-info-title">email:</p>
+                        <p class="font-info">{{ barberData.info.email }}</p>
+                      </div>
+                    </div>
                   </div>
-                </v-flex>
-              </v-layout>
-            </div>
+                </v-card>
+              </v-stepper-content>
 
-            <!-- free for more  -->
-          </div>
-        </v-flex>
+              <v-stepper-content step="2">
+                <v-card>
+                  <v-btn>free for more</v-btn>
+                </v-card>
+              </v-stepper-content>
 
-        <!-- pictures and other  -->
-        <v-flex xs12 sm12 md12 xl12>
-          <div class="barbershop-pictures">
-
-          </div>
-        </v-flex>
-      </v-layout>
-
-      <!--Dialogs-->
+              <v-stepper-content step="3">
+                <v-card>
+                  <v-btn>free for more</v-btn>
+                </v-card>
+              </v-stepper-content>
+            </v-stepper-items>
+          </v-stepper>
+        </div>
+      </div>
+       <!--Dialogs-->
       <add-dialog v-model="dialogs.add" :steps="steps" @event="add" button-text="Crear"></add-dialog>
     </div>
   </v-dialog>
@@ -120,10 +111,12 @@ export default class Barber extends BarberCode {
   @Prop({ default: false }) value!: boolean;
   @Prop({
     default: {
-      name: "ZeroByOne",
-      job: "barber",
-      email: "zerobyone@gmail.com",
-      info: "se especializa en algo",
+      info: {
+        name: "ZeroByOne",
+        job: "barber",
+        email: "zerobyone@gmail.com",
+        stars: 5
+      },
       picture: {
         img: "https://avatars2.githubusercontent.com/u/37275050?s=460&v=4",
         scala: 100,
@@ -139,6 +132,14 @@ export default class Barber extends BarberCode {
       }
     }
   })
-  barber!: any;
+  barberData!: any;
+
+  getStars() {
+    let stars: String = "";
+    for (let index = 0; index < this.barberData.info.stars; index++) {
+      stars += "star ";
+    }
+    return stars;
+  }
 }
 </script>
