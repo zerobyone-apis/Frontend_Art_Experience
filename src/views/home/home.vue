c697e5478bfa88305ecb807d3bb0fdbfc62712bf
Damian <damianezetiel1@gmail.com>
2019-10-01 23:49:06 +0200

    fix home

--- src/views/home/home.vue
+++ src/views/home/home.vue
@@ -1,6 +1,6 @@
 <template transition="slide-x-transition">
   <div id="homePage">
-    <!-- BANNER  -->
+    <!-- banner  -->
     <div class="banner">
       <p class="title font-title">Art Experience</p>
       <p class="sub-title font-title">gestion de Barbershops</p>
@@ -9,16 +9,16 @@
         <v-carousel-item v-for="(pic,index) in pictures" :key="index" :src="pic"></v-carousel-item>
       </v-carousel>
 
-      <div class="div-cut"></div>
+      <!-- <div class="div-cut"></div> -->
     </div>
-
+    <!-- about us -->
     <div class="about_us-box">
       <h1 class="about_us-title font-title">ACERCA DE NOSOTROS</h1>
       <p class="about_us-text font-text pt-4 pb-4">
         Art Experience es una empresa que se destaca en el mercado por brindar servicios de calidad dedicados
         a gestion de barberias y administracion de reservas para clientes.
       </p>
-
+      <!-- list employees  -->
       <div class="employees-list">
         <div class="employee-item" v-for="(item,index) in employees" :key="index">
           <v-img class="employee-img" :src="item.picture.img" aspect-ratio="1"></v-img>
@@ -36,25 +36,21 @@
         </div>
       </div>
     </div>
+    <!-- center decorator -->
+    <div class="decoration-box">
+      <v-parallax class="paralax" src="https://kontinent.se/wp-content/uploads/Barbershop_7.jpg">
+      </v-parallax>
 
-    <v-parallax
-      class="decorator-paralax"
-      src="https://images.unsplash.com/photo-1549271568-e87e07c5406b?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&w=1000&q=80"
-    >
-      <div class="div-cut-top"></div>
-      <div class="div-cut-bottom"></div>
-    </v-parallax>
+      <!-- <div class="div-cut-top"></div>
+      <div class="div-cut-bottom"></div>-->
+    </div>
 
-    <!-- <v-carousel lass="banner-img" :show-arrows="false" hide-delimiter-background hide-delimiters>
-      <v-carousel-item v-for="(pic,index) in pictures" :key="index" :src="pic"></v-carousel-item>
-    </v-carousel>-->
-
+    <!-- services -->
     <div class="services-box">
       <h1 class="services-title font-title">NUESTROS SERVICIOS</h1>
       <p
         class="services-text font-text pt-4 pb-4"
       >Brindamos servicios de calidad que facilitan su trabajo y aumenta su productividad.</p>
-
       <div class="services-list">
         <div class="service-item" v-for="(item,index) in services" :key="index">
           <v-icon class="service-icon">{{ item.icon }}</v-icon>
@@ -64,7 +60,7 @@
         </div>
       </div>
     </div>
-
+    <!-- footer -->
     <div class="footer">
       <p class="logo">Art Experience</p>
       <p class="footer-email">artexperience@gmail.com</p>
@@ -76,7 +72,6 @@
           <v-img class="social-logo" :src="icons.facebook"></v-img>
         </a>
       </div>
-
       <p class="footer-bussiness">© 2019 Art Experience</p>
     </div>
   </div>
@@ -88,7 +83,6 @@ import HomeCode from "./homeCode";
 //style
 import "./homeStyle.scss";
 import "../../styles/fonts.scss";
-
 //components
 import { Component } from "vue-property-decorator";
 
