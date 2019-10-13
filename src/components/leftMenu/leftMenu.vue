772345ffe3fe1cde00b0441856fb48f45b6379fc
Damian <damianezetiel1@gmail.com>
2019-10-13 16:28:40 +0200

    fix left menu

--- src/components/leftMenu/leftMenu.vue
+++ src/components/leftMenu/leftMenu.vue
@@ -1,25 +1,28 @@
 <template v-cloak>
-  <v-navigation-drawer id="drawer-left" dark temporary fixed left v-model="drawer" color="rgba(0, 0, 0, 0.685)">
-    <v-list subheader>
-      <v-list v-if="filteredConf.length > 0">
-        <!-- <v-list-tile
-          v-for="(item, index) in filteredConf"
-          :key="index"
-          @click="pageRouter(item.route)"
-          class="pb-1 pt-1"
-        >
-          <v-list-tile-action class="list-item">
-            <v-icon color="#DDDDDD">{{ item.icon }}</v-icon>
-          </v-list-tile-action>
+  <v-navigation-drawer
+    id="drawer-left"
+    dark
+    temporary
+    fixed
+    left
+    v-model="drawer"
+    color="rgba(0, 0, 0, 0.685)"
+  >
+    <v-list-item
+      v-for="(item, index) in filteredConf"
+      :key="index"
+      @click="pageRouter(item.route)"
+      class="pb-1 pt-1"
+    >
+      <v-list-item-content>
+        <v-list-item-action class="list-item">
+          <v-icon color="#DDDDDD">{{ item.icon }}</v-icon>
+        </v-list-item-action>
+        <v-list-item-title>{{ item.name }}</v-list-item-title>
+      </v-list-item-content>
+    </v-list-item>
 
-          <v-list-tile-content>
-            <v-list-tile-title :style="{ 'color' : '#DDDDDD' }">
-              <span>{{ $i18n.t(item.name) }}</span>
-            </v-list-tile-title>
-          </v-list-tile-content>
-        </v-list-tile> -->
-      </v-list>
-    </v-list>
+
 
     <v-btn small fab class="settings">
       <v-icon color="#BDBDBD">settings</v-icon>
@@ -28,7 +31,7 @@
 </template>
 
 <script lang="ts">
-import Vuetify from 'vuetify/lib'
+import Vuetify from "vuetify/lib";
 import LeftMenuCode from "./leftMenuCode";
 import "./leftMenuStyle.scss";
 
