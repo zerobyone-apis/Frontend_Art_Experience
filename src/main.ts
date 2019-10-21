9f1e23f30ee5836ebb417ff6a34e68e57649c66d
Damian <damianezetiel1@gmail.com>
2019-10-21 02:13:18 +0200

    fix main i18n

--- src/main.ts
+++ src/main.ts
@@ -3,7 +3,7 @@ import vuetify from "./plugins/vuetify";
 import App from "./app/app.vue";
 import router from "./router";
 import store from "./store";
-import VueI18n from "vue-i18n";
+// import VueI18n from "vue-i18n";
 import Locale from "./locale";
 import Croppa from 'vue-croppa';
 import 'vue-croppa/dist/vue-croppa.css';
@@ -14,17 +14,17 @@ import BackToTop from 'vue-backtotop'
  
 Vue.use(BackToTop)
 Vue.use(Croppa);
-Vue.use(VueI18n);
+// Vue.use(VueI18n);
 AOS.init({ disable: "phone" });
 
 
-const i18n = new VueI18n({
-  locale: store.state.lang, // set locale
-  messages: {
-    en: Locale.en,
-    es: Locale.es
-  }
-});
+// const i18n = new VueI18n({
+//   locale: store.state.lang, // set locale
+//   messages: {
+//     en: Locale.en,
+//     es: Locale.es
+//   }
+// });
 
 Vue.config.productionTip = false;
 
@@ -32,10 +32,10 @@ new Vue({
   vuetify,
   router,
   store,
-  i18n,
+  // i18n,
   render: h => h(App)
 }).$mount("#app");
 
 export default {
-  i18n
+  // i18n
 };
