93986e35e04d8dc84d3e88a6c0b587754f9a6116
Damian <damianezetiel1@gmail.com>
2020-04-09 20:59:57 -0300

    create component customDivider

---
+++ src/components/customDivider/customDivider.vue
@@ -0,0 +1,18 @@
+<template>
+  <div :class="`custom-divider_${align}`">
+    <v-parallax class="paralax" :src="img"></v-parallax>
+  </div>
+</template>
+
+<script lang="ts">
+import "./customDivider.scss";
+import "../../styles/fonts.scss";
+import { Component, Prop } from "vue-property-decorator";
+import vue from "vue";
+
+@Component({})
+export default class CustomDivider extends vue {
+  @Prop({ default: "" }) img!: string;
+  @Prop({ default: "left" }) align!: string;
+}
+</script>
