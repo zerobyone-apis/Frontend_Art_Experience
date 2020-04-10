8a4bba11dbb8646b697853b216cbb96c1fc347af
Damian <damianezetiel1@gmail.com>
2020-04-09 21:00:09 -0300

    create component customFooter

---
+++ src/components/customFooter/customFooter.vue
@@ -0,0 +1,35 @@
+<template>
+  <div class="footer">
+    <p class="logo">{{ appName }}</p>
+    <p class="footer-email">{{ email }}</p>
+    <div class="footer-social">
+      <a href>
+        <v-img
+          class="social-logo footer-social-logo"
+          :src="require('../../assets/icons/instagram.png')"
+        ></v-img>
+      </a>
+      <a href>
+        <v-img
+          class="social-logo footer-social-logo"
+          :src="require('../../assets/icons/facebook.png')"
+        ></v-img>
+      </a>
+    </div>
+    <p class="footer-bussiness">{{ bussiness }}</p>
+  </div>
+</template>
+
+<script lang="ts">
+import "./customFooter.scss";
+import "../../styles/fonts.scss";
+import { Component, Prop } from "vue-property-decorator";
+import vue from "vue";
+
+@Component({})
+export default class CustomFooter extends vue {
+  @Prop({ default: "app name" }) appName!: string;
+  @Prop({ default: "email" }) email!: string;
+  @Prop({ default: "bussiness" }) bussiness!: string;
+}
+</script>
