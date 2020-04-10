5218b034c9acdf05a6f9b55f4300f39df8214f39
Damian <damianezetiel1@gmail.com>
2020-04-09 21:00:51 -0300

    fixes in toolbar

--- src/components/toolbar/toolbarCode.ts
+++ src/components/toolbar/toolbarCode.ts
@@ -1,7 +1,5 @@
-import { Component, Prop, Vue } from 'vue-property-decorator';
 import MenuItems from '../menuItems';
 
-@Component({})
 export default class ToolbarCode extends MenuItems {
   private reservationDialog: boolean = false;
 }
