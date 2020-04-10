62d27b177c8f2dc5ea64f1e079ab5b1ea2e4b5e8
Damian <damianezetiel1@gmail.com>
2020-04-09 21:00:43 -0300

    create component socialBar

---
+++ src/components/socialBar/socialBar.vue
@@ -0,0 +1,28 @@
+<template>
+  <div class="item-social">
+    <a :href="urls.instagram" v-if="urls.instagram">
+      <v-img
+        class="item-social-logo social-logo"
+        :src="require('../../assets/icons/instagram.png')"
+      ></v-img>
+    </a>
+    <a :href="urls.facebook">
+      <v-img class="item-social-logo social-logo" :src="require('../../assets/icons/facebook.png')"></v-img>
+    </a>
+    <a :href="urls.whatsapp">
+      <v-img class="item-social-logo social-logo" :src="require('../../assets/icons/whatsapp.png')"></v-img>
+    </a>
+  </div>
+</template>
+
+<script lang="ts">
+import "./socialBar.scss";
+import "../../styles/fonts.scss";
+import { Component, Prop } from "vue-property-decorator";
+import vue from "vue";
+
+@Component({})
+export default class SocialBar extends vue {
+  @Prop({ default: undefined }) urls!: any;
+}
+</script>
